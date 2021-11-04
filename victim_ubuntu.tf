05634f974faf432b57b42599301d8d9e883e4f2d
para93 <92060430+para93@users.noreply.github.com>
2021-11-04 14:28:44 -0700

    Update victim_ubuntu.tf

--- victim_ubuntu.tf
+++ victim_ubuntu.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "allow_http_ssh" {
       from_port        = 22
       to_port          = 80
       protocol         = "tcp"
-      cidr_blocks      = [victim-network-vpc-cidr]  
+      cidr_blocks      = "$[var.victim-network-vpc-cidr]"  
     }
   ]
 
