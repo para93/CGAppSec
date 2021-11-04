9270f07a95bb144851384834668cccba3c306045
para93 <92060430+para93@users.noreply.github.com>
2021-11-04 13:37:21 -0700

    Create victim_ubuntu.tf

---
+++ victim_ubuntu.tf
@@ -0,0 +1,31 @@
+# Create Security Group to access web
+
+resource "aws_security_group" "allow_http_ssh" {
+  name        = "allow_http_ssh"
+  description = "Allow http_ssh inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress = {
+      description      = "http to VPC"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = [victim-network-vpc-cidr]
+    }
+    
+    ingress = {
+      description      = "ssh to VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      security_groups = ["${aws_security_group.allow_http_ssh.id}"]
+    }
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  ]
