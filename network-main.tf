a80100432412a73d346800158fb1d3aea1f2a029
para93 <92060430+para93@users.noreply.github.com>
2021-11-04 11:27:37 -0700

    Update network-main.tf

--- network-main.tf
+++ network-main.tf
@@ -46,6 +46,54 @@ resource "aws_subnet" "public-subnet-2" {
   }
 }
 
+# Create Route Table and Add Public Route
+# terraform aws create route table
+resource "aws_route_table" "public-route-table" {
+  vpc_id       = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet-gateway.id
+  }
+
+  tags       = {
+    Name     = "Default Public Route Table"
+  }
+}
+
+# Associate Public Subnet 1 to "Public Route Table"
+# terraform aws associate subnet with route table
+resource "aws_route_table_association" "public-subnet-1-route-table-association" {
+  subnet_id           = aws_subnet.public-subnet-1.id
+  route_table_id      = aws_route_table.public-route-table.id
+}
+
+# Associate Public Subnet 2 to "Public Route Table"
+# terraform aws associate subnet with route table
+resource "aws_route_table_association" "public-subnet-2-route-table-association" {
+  subnet_id           = aws_subnet.public-subnet-2.id
+  route_table_id      = aws_route_table.public-route-table.id
+}
+
+# Create Private Subnet 1
+# terraform aws create subnet
+resource "aws_subnet" "private-subnet-1" {
+  vpc_id                   = 
+  cidr_block               = 
+  availability_zone        = 
+  map_public_ip_on_launch  = 
+
+  tags      = {
+    Name    =
+  }
+}
+
+
+
+
+
+
+
 
 # Create the victim network VPC
 resource "azurerm_virtual_network" "victim-network-vpc" {
