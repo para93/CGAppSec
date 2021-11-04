74aaccc80fdc206ac3b61d681b9ac8d87f19ff56
para93 <92060430+para93@users.noreply.github.com>
2021-11-04 15:33:28 -0700

    Update victim_ubuntu.tf

--- victim_ubuntu.tf
+++ victim_ubuntu.tf
@@ -4,7 +4,7 @@ data "template_file" "userdata_setup" {
   template = "${file("userdata_setup.template")}"
 
   vars  = {
-    name       = "${var.username}"
+    name      = "${var.username}"
     token     = "${var.token}"
     logic = "${file("vuln_bootstrap.sh")}"
   }
