c7ce91ed0d6b32b659e76cbfcf7f022d55621f15
para93 <92060430+para93@users.noreply.github.com>
2021-11-04 12:12:03 -0700

    Update network-main.tf

--- network-main.tf
+++ network-main.tf
@@ -7,6 +7,8 @@ resource "aws_vpc" "vpc" {
 
   tags      = {
     Name    = "AppSec VPC"
+    application = var.victim_company
+    environment = var.environment
   }
 }
 
@@ -125,24 +127,4 @@ resource "aws_subnet" "private-subnet-4" {
   tags      = {
     Name    = "Private Subnet 4 | Database Tier"
   }
-}
-
-# Create the victim network VPC
-resource "azurerm_virtual_network" "victim-network-vpc" {
-  name                = "${var.victim_company}-vpc"
-  address_space       = [var.victim-network-vpc-cidr]
-  resource_group_name = azurerm_resource_group.victim-network-rg.name
-  location            = azurerm_resource_group.victim-network-rg.location
-  tags = {
-    application = var.victim_company
-    environment = var.environment
-  }
-}
-
-# Create a victim subnet for Network
-resource "azurerm_subnet" "victim-network-subnet" {
-  name                 = "${var.victim_company}-subnet"
-  address_prefix       = var.victim-network-subnet-cidr
-  virtual_network_name = azurerm_virtual_network.victim-network-vpc.name
-  resource_group_name  = azurerm_resource_group.victim-network-rg.name
 }
