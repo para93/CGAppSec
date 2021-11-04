bd242a6709a32c5882fd3d9205d96d52991f244a
para93 <92060430+para93@users.noreply.github.com>
2021-11-04 15:21:12 -0700

    Update victim_ubuntu.tf

--- victim_ubuntu.tf
+++ victim_ubuntu.tf
@@ -1,30 +1,43 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
+resource "aws_security_group" "appsec" {
+  name        = "appsec-sg"
+  description = "Allow web traffics"
   vpc_id      = aws_vpc.vpc.id
 
-  ingress = [
-    {
-      description      = "TLS from VPC"
-      from_port        = 443
-      to_port          = 443
-      protocol         = "tcp"
-      cidr_blocks      = "$[var.victim-network-vpc-cidr]"
-      ipv6_cidr_blocks = "$[var.victim-network-vpc-cidr-ipv6]"
-    }
-  ]
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  egress = [
-    {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
-  ]
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description      = "for all outgoing traffics"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+
+  }
 
   tags = {
-    Name = "allow_tls"
+    Name = "AppSec sg-for-subnet"
   }
 }
