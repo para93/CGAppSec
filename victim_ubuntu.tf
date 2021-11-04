0531f8907f41774261e89ae1bff7547b57f23552
para93 <92060430+para93@users.noreply.github.com>
2021-11-04 14:20:30 -0700

    Update victim_ubuntu.tf

--- victim_ubuntu.tf
+++ victim_ubuntu.tf
@@ -5,27 +5,27 @@ resource "aws_security_group" "allow_http_ssh" {
   description = "Allow http_ssh inbound traffic"
   vpc_id      = aws_vpc.vpc.id
 
-  ingress = {
-      description      = "http to VPC"
-      from_port        = 80
+  ingress = [
+    
+    {
+      description      = "http_ssh to VPC"
+      from_port        = 22
       to_port          = 80
       protocol         = "tcp"
-      cidr_blocks      = [victim-network-vpc-cidr]
-    
-    
-  ingress = {
-      description      = "ssh to VPC"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      security_groups = ["${aws_security_group.allow_http_ssh.id}"]
+      cidr_blocks      = [victim-network-vpc-cidr]  
     }
+  ]
 
-  egress = {
-    
+  egress = [
+    {
       from_port        = 0
       to_port          = 0
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
     }
+  ]
+    
+  tags = {
+    Name = "allow_http_ssh"
+  }
 }
