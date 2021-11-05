5f376d0f9eef89fc340e3b8480734ba997b242f7
para93 <92060430+para93@users.noreply.github.com>
2021-11-04 19:03:05 -0700

    Update victim_ubuntu.tf

--- victim_ubuntu.tf
+++ victim_ubuntu.tf
@@ -10,47 +10,134 @@ data "template_file" "userdata_setup" {
   }
 }
 
-#Create Security Group Ingress-Egress
-resource "aws_security_group" "appsec" {
-  name        = "appsec-sg"
-  description = "Allow web traffics"
+# Create Security Group for the Application Load Balancer
+# terraform aws create security group
+resource "aws_security_group" "alb-security-group" {
+  name        = "LB AppSec Security Group"
+  description = "Allow HTTP/SSH Access on Port 80/22"
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "Allow HTTP Access on Port 80"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "Allow SSH Access on Port 22"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
-    description      = "for all outgoing traffics"
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-
   }
 
-  tags = {
-    Name = "AppSec sg-for-subnet"
+  tags   = {
+    Name = "LB AppSec Security Group"
+  }
+}
+
+# Create Security Group for the Bastion Host aka Jump Box
+# terraform aws create security group
+resource "aws_security_group" "ssh-security-group" {
+  name        = "Bastion AppSec Security Group"
+  description = "Allow SSH to Bastion"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description      = "Allow SSH to Bastion"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["${var.ssh-location}"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags   = {
+    Name = "Bastion AppSec Security Group"
+  }
+}
+
+# Create Security Group for the Web Server
+# terraform aws create security group
+resource "aws_security_group" "webserver-security-group" {
+  name        = "Juice-shop Security Group"
+  description = "Allow HTTP/HTTPS access on Port 80/443 via ALB and SSH on Port 22 via SG"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description      = "Allow HTTP to Juice Shop"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    security_groups  = ["${aws_security_group.alb-security-group.id}"]
+  }
+
+  ingress {
+    description      = "Allow 443 to Juice Shop"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    security_groups  = ["${aws_security_group.alb-security-group.id}"]
+  }
+
+  ingress {
+    description      = "Allow SSH to Juice Shop"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    security_groups  = ["${aws_security_group.ssh-security-group.id}"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags   = {
+    Name = "Juice Shop Security Group"
+  }
+}
+
+# Create Security Group for the Database
+# terraform aws create security group
+resource "aws_security_group" "database-security-group" {
+  name        = "Database Security Group"
+  description = "Enable MYSQL/Aurora on Port 3306"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description      = "Allow MYSQL on port 3306"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    security_groups  = ["${aws_security_group.webserver-security-group.id}"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags   = {
+    Name = "Database Security Group"
   }
 }
